da923f60dde3775619b27b52fbdab4555e30eef9
James Valente <j_valente@salemstate.edu>
2012-02-22 20:55:30 -0500

    Grid creation and population with placeholder, start, and goal markers

--- src/main.py
+++ src/main.py
@@ -8,48 +8,56 @@
 #-List of Robots        #
 #-List of Obstacles     #
 #########################
-
+from random import randint
 def main():
     numRobots = 10
-    size = (25,25)
-    print "Finding paths for " + str(numRobots) + \
-        " robots on a grid size of " + str(size)
-    y = Robot(2,1)
-    print y
-    
-def makeRobots(size):
+    size = (randint(10,51),randint(10,51))
+    Robot.size = size
+    print "Finding paths for {0} robots on a {1[0]!s}x{1[1]!s} grid.\n".format(numRobots, size)
+    roboArray = makeRobots(size,numRobots)
+    for robot in roboArray:
+        print robot
+        
+def makeRobots(size, numRobots):
+    height, width = size
     arr = []
-    for robot in range():
-        arr[robot] = Robot(1,1)
+    for i in range(numRobots):
+        start = (randint(0,height), randint(0,width))
+        goal = (randint(0,height), randint(0,width))
+        arr.append(Robot(start,goal))
     return  arr
-
-    return    
+   
 def wavefront():
     #Placeholder for wavefront algorithm
-    return
+    pass
 #Create the robot objects
 class Robot:
+    population = 0
     def __init__(self, start, goal):
         self.start = start
         self.goal = goal
         self.color = self.genColor()
-        self.matrix = 0  # Todo, implement matrix
+        self.matrix = self.makeGrid(self.size, self.start, self.goal)  # Todo, implement matrix
+        self.ID = Robot.population
+        Robot.population +=1
     def checkPoint(self, row, col):    
-            return #
+            return self.matrix[row][col]
     def __str__(self):
-        return "Start: " + str(self.start) + "\nGoal: " + str(self.goal) \
-             + "\nColor: " + str(self.color)           
+        return "ID : {id!s}\nStart: {st!s}\nGoal: {gl!s}\nColor: {cl!s}\n" \
+                .format(id=self.ID, st=self.start, gl=self.goal, cl=self.color)                
     @staticmethod
     def genColor():
-        from random import randint
         x = randint(0, 16777215)
         return hex(x)
-class Grid:
-    def __init__(self, rows, cols, obst): 
-        self.rows = rows
-        self.cols = cols
-        self.obst = obst      
+    def makeGrid(self, size, start, goal):
+        #Create Grid first
+        height, width = (size)
+        sr, sc = start
+        gr, gc = goal
+        grid = [['?'] * width for i in range(height)]
+        grid[sr-1][sc-1] = "S"
+        grid[gr-1][gc-1] = "G"
+        return grid
     
 if __name__ == '__main__':
-    size = (25,25)
     main()    
