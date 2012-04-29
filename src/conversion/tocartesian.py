2035a6cdf50098a800c0ebd8eca978646b7e60e2
James Valente <j_valente@salemstate.edu>
2012-04-29 19:50:22 -0400

    Fixing error in point conversion that flipped Y axis

--- src/conversion/tocartesian.py
+++ src/conversion/tocartesian.py
@@ -5,6 +5,6 @@ def convertPoint(point, size):
     row, col = point
     #We must output the points for use on a Cartesian coordinate system
     x = col - size[1] / 2  # A grid with n columns spans from -(n/2) to (n/2)
-    y = row - size[0] / 2  # A grid with m rows spans from -(m/2) to (m/2)
+    y = size[0] / 2 - row  # A grid with m rows spans from -(m/2) to (m/2)
     z = 0 #Z will always be zero on our 2D matrix
     return (x, y, z)
